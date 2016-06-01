7d276fdd7475b5a02a789126625ef39a390cbaa6
kinshasa <liushaobo2005@163.com>
2016-06-01 14:54:13 +0800

    Create CommonLib CommonView app.

---
+++ CommonView/app/src/main/java/comview/net/xicp/liushaobo/commonview/Dialog/BaseDialog.java
@@ -0,0 +1,15 @@
+package comview.net.xicp.liushaobo.commonview.Dialog;
+
+import android.app.Dialog;
+import android.content.Context;
+
+/**
+ * Created by liushaobo.xicp.net on 2016/5/27.
+ */
+public  class BaseDialog extends Dialog{
+
+    BaseDialog(Context context){
+
+        super(context);
+    }
+}
---
+++ CommonView/app/src/main/java/comview/net/xicp/liushaobo/commonview/Dialog/NoTitleDialog.java
@@ -0,0 +1,16 @@
+package comview.net.xicp.liushaobo.commonview.Dialog;
+
+import android.content.Context;
+
+/**
+ * Created by liushaobo.xicp.net on 2016/5/27.
+ */
+public class NoTitleDialog extends BaseDialog {
+
+    public NoTitleDialog(Context context){
+        super(context);
+
+    }
+
+
+}
