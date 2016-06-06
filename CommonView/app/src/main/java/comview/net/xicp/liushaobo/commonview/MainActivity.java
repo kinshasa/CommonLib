387b84c9b5190c1d7e99ef6d9d5a2a4bc1db9648
kinshasa <liushaobo2005@163.com>
2016-06-06 15:00:25 +0800

    Revert "test"
    This reverts commit 2407a263f5567d0e9e158518192e3d111e817a23.

--- CommonView/app/src/main/java/comview/net/xicp/liushaobo/commonview/MainActivity.java
+++ CommonView/app/src/main/java/comview/net/xicp/liushaobo/commonview/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.v("liushaobo","onCreate");
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
