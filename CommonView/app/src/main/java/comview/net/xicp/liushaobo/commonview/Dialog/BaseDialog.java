70aba5cb6d2e56d9e0ef812f541d13cce5822907
kinshasa <liushaobo2005@163.com>
2016-06-06 12:11:59 +0800

    Rejected commit: Default

--- CommonView/app/src/main/java/comview/net/xicp/liushaobo/commonview/Dialog/BaseDialog.java
+++ CommonView/app/src/main/java/comview/net/xicp/liushaobo/commonview/Dialog/BaseDialog.java
@@ -2,14 +2,28 @@ package comview.net.xicp.liushaobo.commonview.Dialog;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.view.Window;
 
 /**
  * Created by liushaobo.xicp.net on 2016/5/27.
  */
 public  class BaseDialog extends Dialog{
 
-    BaseDialog(Context context){
-
+    public BaseDialog(Context context) {
         super(context);
+
+        //去掉系统对话框的title和边框背景
+        getWindow().setBackgroundDrawable(new ColorDrawable(0));
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+    }
+    public BaseDialog(Context context,int theme) {
+        super(context,theme);
+
+        //去掉系统对话框的title和边框背景
+        getWindow().setBackgroundDrawable(new ColorDrawable(0));
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
     }
 }
--- CommonView/app/src/main/java/comview/net/xicp/liushaobo/commonview/MainActivity.java
+++ CommonView/app/src/main/java/comview/net/xicp/liushaobo/commonview/MainActivity.java
@@ -1,19 +1,47 @@
 package comview.net.xicp.liushaobo.commonview;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-import comview.net.xicp.liushaobo.commonview.Dialog.NoTitleDialog;
+import comview.net.xicp.liushaobo.commonview.Dialog.ConfirmDialog;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+    private Context context;
+    private ConfirmDialog confirmDialog;
+
+    private Button btnShowDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.v("liushaobo","onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        NoTitleDialog noTitleDialog = new NoTitleDialog(getApplicationContext());
-        //noTitleDialog.show();
+        context = this;
+        btnShowDialog = (Button)findViewById(R.id.btnShowDialog);
+        btnShowDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showConfirmDialog();
+            }
+        });
+
+    }
+
+    private void showConfirmDialog(){
+        if(confirmDialog == null){
+            confirmDialog = new ConfirmDialog(context);
+        }
+        if(confirmDialog.isShowing()){
+            confirmDialog.hide();
+        }else{
+            confirmDialog.show();
+        }
     }
 
 
