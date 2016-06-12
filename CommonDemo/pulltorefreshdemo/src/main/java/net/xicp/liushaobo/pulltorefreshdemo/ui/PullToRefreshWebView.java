8b0b4711df96f0dfc6e652b9d1221744ed48b0df
kinshasa <liushaobo2005@163.com>
2016-06-12 12:16:34 +0800

    submit

---
+++ CommonDemo/pulltorefreshdemo/src/main/java/net/xicp/liushaobo/pulltorefreshdemo/ui/PullToRefreshWebView.java
@@ -0,0 +1,71 @@
+package net.xicp.liushaobo.pulltorefreshdemo.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.FloatMath;
+import android.webkit.WebView;
+
+/**
+ * 封装了WebView的下拉刷新
+ * 
+ * @author Li Hong
+ * @since 2013-8-22
+ */
+public class PullToRefreshWebView extends PullToRefreshBase<WebView> {
+    /**
+     * 构造方法
+     * 
+     * @param context context
+     */
+    public PullToRefreshWebView(Context context) {
+        this(context, null);
+    }
+    
+    /**
+     * 构造方法
+     * 
+     * @param context context
+     * @param attrs attrs
+     */
+    public PullToRefreshWebView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+    
+    /**
+     * 构造方法
+     * 
+     * @param context context
+     * @param attrs attrs
+     * @param defStyle defStyle
+     */
+    public PullToRefreshWebView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    /**
+     * @see net.xicp.liushaobo.pulltorefreshdemo.ui.PullToRefreshBase#createRefreshableView(Context, AttributeSet)
+     */
+    @Override
+    protected WebView createRefreshableView(Context context, AttributeSet attrs) {
+        WebView webView = new WebView(context);
+        return webView;
+    }
+
+    /**
+     * @see net.xicp.liushaobo.pulltorefreshdemo.ui.PullToRefreshBase#isReadyForPullDown()
+     */
+    @Override
+    protected boolean isReadyForPullDown() {
+        return mRefreshableView.getScrollY() == 0;
+    }
+
+    /**
+     * @see net.xicp.liushaobo.pulltorefreshdemo.ui.PullToRefreshBase#isReadyForPullUp()
+     */
+    @Override
+    protected boolean isReadyForPullUp() {
+        //float exactContentHeight = FloatMath.floor(mRefreshableView.getContentHeight() * mRefreshableView.getScale());
+        float exactContentHeight = (float)Math.floor(mRefreshableView.getContentHeight() * mRefreshableView.getScale());
+        return mRefreshableView.getScrollY() >= (exactContentHeight - mRefreshableView.getHeight());
+    }
+}
